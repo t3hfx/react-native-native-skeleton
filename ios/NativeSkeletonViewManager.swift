b821d0d51ba4c3eb42f22dcbfe969b156a651be6
Kirill Kozlov <k.kozlov@p-group.ru>
2025-01-19 19:09:48 +0300

    feat: direction is done on iOS

--- ios/NativeSkeletonViewManager.swift
+++ ios/NativeSkeletonViewManager.swift
@@ -18,6 +18,7 @@ class NativeSkeletonView : UIView {
   private var baseColor: UIColor = UIColor.clear // Default color
   private var secondaryColor: UIColor = UIColor.clear // Default color
   private var _duration: Double = 0.0 // Default duration
+  private var _direction: GradientDirection = .topLeftBottomRight // Default direction
 
 
   // Expose baseBackgroundColor property to be set from JavaScript
@@ -48,7 +49,6 @@ class NativeSkeletonView : UIView {
   // Expose visible property to be set from JavaScript
   @objc var visible: Bool = true {
     didSet {
-      isSkeletonVisible = visible
       checkAndUpdateSkeleton()
       if !visible {
         self.setSkeletonVisibility(isVisible: false, baseColor: baseColor, secondaryColor: nil)
@@ -56,11 +56,34 @@ class NativeSkeletonView : UIView {
     }
   }
 
+  // Expose visible property to be set from JavaScript
+  @objc var direction: String = "" {
+    didSet {
+      switch direction {
+        case "leftRight":
+            self._direction = .leftRight
+        case "rightLeft":
+            self._direction = .rightLeft
+        case "topBottom":
+            self._direction = .topBottom
+        case "bottomTop":
+            self._direction = .bottomTop
+        case "topLeftBottomRight":
+            self._direction = .topLeftBottomRight
+        case "bottomRightTopLeft":
+            self._direction = .bottomRightTopLeft
+        default:
+            self._direction = .topLeftBottomRight
+      }
+      checkAndUpdateSkeleton()
+    }
+  }
+
   private func checkAndUpdateSkeleton() {
   // Ensure that we have secondaryBackgroundColor and baseBackgroundColor from react native then update the skeleton
-    if isSkeletonVisible && !secondaryBackgroundColor.isEmpty && !baseBackgroundColor.isEmpty && (_duration > 0.0) {
+    if isSkeletonVisible && !secondaryBackgroundColor.isEmpty && !baseBackgroundColor.isEmpty && (_duration > 0.0) && direction != "" {
       let customGradient = SkeletonGradient(baseColor: baseColor, secondaryColor: secondaryColor)
-      let animation = SkeletonAnimationBuilder().makeSlidingAnimation(withDirection: GradientDirection.topLeftBottomRight, duration: _duration)
+      let animation = SkeletonAnimationBuilder().makeSlidingAnimation(withDirection: self._direction, duration: _duration)
       self.updateAnimatedGradientSkeleton(usingGradient: customGradient, animation: animation)
     } 
   }
